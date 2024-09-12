835e367080430deeff6bb18568acd4f51674448b
AlanPratama <pralan76@gmail.com>
2024-09-13 00:45:14 +0700

    [UX]: add time in kerjain aja section

--- src/screens/auth/HomeScreen.jsx
+++ src/screens/auth/HomeScreen.jsx
@@ -13,10 +13,13 @@ export default function HomeScreen() {
     }
   };
 
-  const l = [{}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {},]
+  const l = [{}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}];
 
   return (
-    <ScrollView contentContainerStyle={{ paddingBottom: 110 }} className="bg-white min-h-screen ">
+    <ScrollView
+      contentContainerStyle={{ paddingBottom: 110 }}
+      className="bg-white min-h-screen "
+    >
       <View className="bg-[#fff] px-3 pt-4 flex-row justify-between items-center">
         <Text className="text-[26px] font-bold text-[#4f6def]">
           Kerjain Aja
@@ -119,14 +122,21 @@ export default function HomeScreen() {
           </Text>
           {/* <Ionicons name="chevron-forward-outline" size={18} /> */}
         </View>
-        <ScrollView className="py-3.5" horizontal={true} showsHorizontalScrollIndicator={false}>
-          {
-            l.map((_, i) => (
-              <TouchableOpacity key={i} className="bg-blue-100 rounded-full px-3.5 py-1 mr-2 justify-center items-center">
-                <Text className="text-[14.5px] font-semibold text-[#4f6def]">Kurir</Text>
-              </TouchableOpacity>
-            )) 
-          }
+        <ScrollView
+          className="py-3.5"
+          horizontal={true}
+          showsHorizontalScrollIndicator={false}
+        >
+          {l.map((_, i) => (
+            <TouchableOpacity
+              key={i}
+              className="bg-blue-100 rounded-full px-3.5 py-1 mr-2 justify-center items-center"
+            >
+              <Text className="text-[14.5px] font-semibold text-[#4f6def]">
+                Kurir
+              </Text>
+            </TouchableOpacity>
+          ))}
         </ScrollView>
       </View>
 
@@ -138,43 +148,45 @@ export default function HomeScreen() {
           {/* <Ionicons name="chevron-forward-outline" size={18} /> */}
         </View>
 
-        <TouchableOpacity activeOpacity={0.5} className="my-3.5 flex-row justify-start items-start gap-x-2.5">
-          <Image source={{ uri: "https://www.waifu.com.mx/wp-content/uploads/2023/05/Rei-Ayanami-20.jpg" }} alt="" className="w-[88px] h-[88px] border border-gray-200 rounded-xl"/>
+        <TouchableOpacity
+          activeOpacity={0.5}
+          className="my-3.5 flex-row justify-start items-start gap-x-2.5"
+        >
+          <Image
+            source={{
+              uri: "https://www.waifu.com.mx/wp-content/uploads/2023/05/Rei-Ayanami-20.jpg",
+            }}
+            alt=""
+            className="w-[88px] h-[88px] border border-gray-200 rounded-xl"
+          />
           <View className="w-[68%]">
-            <Text className="text-sm font-bold text-[#4f6def]">Serba Bisa</Text>
-            <Text numberOfLines={1} className="text-lg font-bold text-[#343434]">Kuburin kucing aku</Text>
-            <Text numberOfLines={2} className="text-sm font-normal text-[#343434]">halo banh, jadi ada kucing aku yang namanya Boni, dia kucing kesayanganku, tolong bantu kuburin dong :(</Text>
+            <View className="flex-row justify-between items-center">
+              <Text className="text-sm font-bold text-[#4f6def]">
+                Serba Bisa
+              </Text>
+              <View className="flex-row justify-center items-center gap-x-1">
+                <Text className="text-sm font-normal text-[#343434]">
+                  2 jam lalu
+                </Text>
+                <Ionicons name="time-outline" size={18} />
+              </View>
+            </View>
+            <Text
+              numberOfLines={1}
+              className="text-[17px] font-bold text-[#343434]"
+            >
+              Kuburin kucing aku
+            </Text>
+            <Text
+              numberOfLines={2}
+              className="text-sm font-normal text-[#343434]"
+            >
+              halo banh, jadi ada kucing aku yang namanya Boni, dia kucing
+              kesayanganku, tolong bantu kuburin dong :(
+            </Text>
           </View>
         </TouchableOpacity>
-
-        <TouchableOpacity activeOpacity={0.5} className="my-3.5 flex-row justify-start items-start gap-x-2.5">
-          <Image source={{ uri: "https://www.waifu.com.mx/wp-content/uploads/2023/05/Rei-Ayanami-20.jpg" }} alt="" className="w-[88px] h-[88px] border border-gray-200 rounded-xl"/>
-          <View className="w-[68%]">
-            <Text className="text-sm font-bold text-[#4f6def]">Serba Bisa</Text>
-            <Text numberOfLines={1} className="text-lg font-bold text-[#343434]">Kuburin kucing aku</Text>
-            <Text numberOfLines={2} className="text-sm font-normal text-[#343434]">halo banh, jadi ada kucing aku yang namanya Boni, dia kucing kesayanganku, tolong bantu kuburin dong :(</Text>
-          </View>
-        </TouchableOpacity>
-
-        <TouchableOpacity activeOpacity={0.5} className="my-3.5 flex-row justify-start items-start gap-x-2.5">
-          <Image source={{ uri: "https://www.waifu.com.mx/wp-content/uploads/2023/05/Rei-Ayanami-20.jpg" }} alt="" className="w-[88px] h-[88px] border border-gray-200 rounded-xl"/>
-          <View className="w-[68%]">
-            <Text className="text-sm font-bold text-[#4f6def]">Serba Bisa</Text>
-            <Text numberOfLines={1} className="text-lg font-bold text-[#343434]">Kuburin kucing aku</Text>
-            <Text numberOfLines={2} className="text-sm font-normal text-[#343434]">halo banh, jadi ada kucing aku yang namanya Boni, dia kucing kesayanganku, tolong bantu kuburin dong :(</Text>
-          </View>
-        </TouchableOpacity>
-        <TouchableOpacity activeOpacity={0.5} className="my-3.5 flex-row justify-start items-start gap-x-2.5">
-          <Image source={{ uri: "https://www.waifu.com.mx/wp-content/uploads/2023/05/Rei-Ayanami-20.jpg" }} alt="" className="w-[88px] h-[88px] border border-gray-200 rounded-xl"/>
-          <View className="w-[68%]">
-            <Text className="text-sm font-bold text-[#4f6def]">Serba Bisa</Text>
-            <Text numberOfLines={1} className="text-lg font-bold text-[#343434]">Kuburin kucing aku</Text>
-            <Text numberOfLines={2} className="text-sm font-normal text-[#343434]">halo banh, jadi ada kucing aku yang namanya Boni, dia kucing kesayanganku, tolong bantu kuburin dong :(</Text>
-          </View>
-        </TouchableOpacity>
-
       </View>
-
     </ScrollView>
   );
 }
