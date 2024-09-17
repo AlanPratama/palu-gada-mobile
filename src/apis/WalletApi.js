83a9ce84f2eccf809703fc71f742c5a2b8a9e1a4
AlanPratama <pralan76@gmail.com>
2024-09-18 01:04:53 +0700

    [FEATURE]: implement logic for user top up

--- src/apis/WalletApi.js
+++ src/apis/WalletApi.js
@@ -1,9 +1,23 @@
 import { axiosInstance } from "./axiosInstance";
 
 export default class WalletApi {
-    static async createPayment(data) {
+    static async createPayment(request) {
         try {
-            const { data } = await axiosInstance.post("/payments", data)
+            console.log("request: ",request);
+            
+            const { data } = await axiosInstance.post("/payments", request)
+            return data
+        } catch (error) {
+            console.error("WalletApi createPayment: ", error);
+        }
+    }
+
+    static async fetchPayment(paymentId) {
+        try {
+            const { data } = await axiosInstance.get(`/payments/${paymentId}/transaction`)
+
+            console.log(data);
+            
             return data
         } catch (error) {
             console.error("WalletApi createPayment: ", error);
