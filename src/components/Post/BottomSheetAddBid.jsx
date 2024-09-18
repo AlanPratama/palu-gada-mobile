fac7c9338b623737f18cd0fb0bd06097f996fdbf
AlanPratama <pralan76@gmail.com>
2024-09-19 03:34:24 +0700

    test

--- src/components/Post/BottomSheetAddBid.jsx
+++ src/components/Post/BottomSheetAddBid.jsx
@@ -1,8 +1,10 @@
-import React, { useState } from 'react';
-import { ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import React from 'react';
+import { Controller, useForm } from 'react-hook-form';
+import { ScrollView, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import RBSheet from 'react-native-raw-bottom-sheet';
+import BidApi from '../../apis/BidApi';
 
-export default function BottomSheetAddBid({ refRBSheet }) {
+export default function BottomSheetAddBid({ refRBSheet, post }) {
   return (
     <View>
         <RBSheet
@@ -30,7 +32,7 @@ export default function BottomSheetAddBid({ refRBSheet }) {
         height={500}
         openDuration={250}
       >
-        <AddBidComp refRBSheet={refRBSheet} />
+        <AddBidComp refRBSheet={refRBSheet} post={post} />
       </RBSheet>
     </View>
   )
@@ -38,12 +40,23 @@ export default function BottomSheetAddBid({ refRBSheet }) {
 
 
 
-const AddBidComp = ({ refRBSheet }) => {
-    const [amount, setAmount] = useState(0)
-    const [message, setMessage] = useState("")
-    
-    const submit = async () => {
+const AddBidComp = ({ refRBSheet, post }) => {
+  
+  const {
+    control,
+    watch,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm();
+
+    const onSubmit = async (data) => {
         refRBSheet.current.close()
+        await BidApi.createBid({
+            postId: post.id,
+            amount: parseInt(data.amount),
+            message: data.message
+        })
     }
 
     return (
@@ -60,39 +73,61 @@ const AddBidComp = ({ refRBSheet }) => {
             <Text style={{ fontWeight: "600", fontSize: 16, marginBottom: 8 }}>
               Harga Penawaran
             </Text>
-            <TextInput
-              value={amount}
-              onChangeText={setAmount}
-              keyboardType='numeric'
-              placeholder="100.000"
-              style={{
-                padding: 10,
-                paddingHorizontal: 16,
-                borderRadius: 10,
-                borderWidth: 1,
-                borderColor: "gray",
+            <Controller
+              control={control}
+              rules={{
+                required: "Harga Penawaran wajib diisi!", validate: (value) => value >= post.budgetMin && value <= post.budgetMax || `Harga penawaran minimal ${post.budgetMin.toLocaleString("id-ID")} dan maksimal ${post.budgetMax.toLocaleString("id-ID")}`
               }}
+              render={({ field: { onChange, value } }) => (
+                <TextInput
+                  value={value}
+                  onChangeText={onChange}
+                  placeholder="Masukkan Harga Penawaran..."
+                  keyboardType="numeric"
+                  style={{
+                    padding: 10,
+                    paddingHorizontal: 16,
+                    borderRadius: 10,
+                    borderWidth: 1,
+                    borderColor: "gray",
+                  }}
+                />
+              )}
+              name="amount"
             />
+            {errors.amount && (
+            <Text style={{ color: "red" }}>{errors.amount.message}</Text>
+          )}
           </View>
 
           <View style={{ paddingHorizontal: 18, marginBottom: 25 }}>
             <Text style={{ fontWeight: "600", fontSize: 16, marginBottom: 8 }}>
               Pesan Kamu
             </Text>
-            <TextInput
-              value={message}
-              onChangeText={setMessage}
-              placeholder="Masukkan Pesan...."
-              multiline
-              numberOfLines={2}
-              style={{
-                padding: 6,
-                paddingHorizontal: 16,
-                borderRadius: 10,
-                borderWidth: 1,
-                borderColor: "gray",
+            <Controller
+              control={control}
+              rules={{
+                required: "Pesan wajib diisi!",
               }}
+              render={({ field: { onChange, value } }) => (
+                <TextInput
+                  value={value}
+                  onChangeText={onChange}
+                  placeholder="Masukkan Pesan..."
+                  style={{
+                    padding: 10,
+                    paddingHorizontal: 16,
+                    borderRadius: 10,
+                    borderWidth: 1,
+                    borderColor: "gray",
+                  }}
+                />
+              )}
+              name="message"
             />
+            {errors.message && (
+            <Text style={{ color: "red" }}>{errors.message.message}</Text>
+          )}
           </View>
             
           <View
@@ -106,7 +141,7 @@ const AddBidComp = ({ refRBSheet }) => {
             }}
           >
             <TouchableOpacity
-              onPress={submit}
+              onPress={handleSubmit(onSubmit)}
               style={{
                 backgroundColor: "#3b82f6",
                 flex: 1,
